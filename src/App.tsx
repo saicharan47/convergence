9d38aec249113046f15968e58fa6c814f7d56681
Sai Charan AK <saicharan.ak477@gmail.com>
2026-09-16 01:14:33 +0530

    fix: guard organizer routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,8 @@ import { OrganizerLoginScreen } from './screens/organizer/OrganizerLoginScreen';
 import { OrganizerDashboardScreen } from './screens/organizer/OrganizerDashboardScreen';
 import { ClueManagementScreen } from './screens/organizer/ClueManagementScreen';
 import { OrganizerLeaderboardScreen } from './screens/organizer/OrganizerLeaderboardScreen';
-
+import { OrganizerGuard } from './components/OrganizerGuard';
 import { AnimatePresence } from 'framer-motion';
-
 import { PageTransition } from './components/PageTransition';
 
 function AppRoutes() {
@@ -31,13 +30,10 @@ function AppRoutes() {
         <Route path="/clue/:id/success" element={<PageTransition><SuccessScreen /></PageTransition>} />
         <Route path="/treasure" element={<PageTransition><TreasureScreen /></PageTransition>} />
         <Route path="/leaderboard" element={<PageTransition><LeaderboardScreen /></PageTransition>} />
-
-        {/* Organizer Routes */}
         <Route path="/organizer" element={<OrganizerLoginScreen />} />
-        <Route path="/organizer/dashboard" element={<OrganizerDashboardScreen />} />
-        <Route path="/organizer/clues" element={<ClueManagementScreen />} />
-        <Route path="/organizer/leaderboard" element={<OrganizerLeaderboardScreen />} />
-
+        <Route path="/organizer/dashboard" element={<OrganizerGuard><OrganizerDashboardScreen /></OrganizerGuard>} />
+        <Route path="/organizer/clues" element={<OrganizerGuard><ClueManagementScreen /></OrganizerGuard>} />
+        <Route path="/organizer/leaderboard" element={<OrganizerGuard><OrganizerLeaderboardScreen /></OrganizerGuard>} />
         <Route path="/profile" element={<Navigate to="/dashboard" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
@@ -46,11 +42,7 @@ function AppRoutes() {
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+  return <BrowserRouter><AppRoutes /></BrowserRouter>;
 }
 
 export default App;
