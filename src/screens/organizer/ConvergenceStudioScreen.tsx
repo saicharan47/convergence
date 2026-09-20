9ddd68b15f75abfd290554b5f546fc23a312a54b
Sai Charan AK <saicharan.ak477@gmail.com>
2026-09-20 21:12:50 +0530

    fix: remove unused studio API import

--- src/screens/organizer/ConvergenceStudioScreen.tsx
+++ src/screens/organizer/ConvergenceStudioScreen.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { OrganizerLayout } from '../../layouts/OrganizerLayout';
 import {
-  adminAssignPair,
   adminAssignSequence,
   adminManualTeamAction,
   adminUpsertCheckpoint,
